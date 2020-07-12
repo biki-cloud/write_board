70939d62cd18dc83073cd0490f976ed2f2647322
biki-cloud <hibiki123123@gmail.com>
2020-07-12 12:46:20 +0900

    update favicon11

--- app.py
+++ app.py
@@ -33,9 +33,10 @@ def index():
 
 @app.route('/favicon.ico') 
 def favicon(): 
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return app.send_static_file("favicon.ico")
+    #return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
     #app.run()
-    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
